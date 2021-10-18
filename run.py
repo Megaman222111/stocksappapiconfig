9cc6c282feabce4631178b75721088c09d994c93
Megh Mistry <mmistry169@gmail.com>
2021-10-18 14:43:36 -0600

    second commit

---
+++ run.py
@@ -0,0 +1,31 @@
+import styvio
+
+data = styvio.get_data('TSLA')
+
+companyDescription = data['companyDescription']
+
+logoURL = data['logoURL']
+
+currentPrice = data['currentPrice']
+
+newsArticle1 = data["newsArticle1"]
+newsArticle2 = data["newsArticle2"]
+newsArticle3 = data["newsArticle3"]
+
+newsLink1 = data["newsLink1"]
+newsLink2 = data["newsLink2"]
+newsLink3 = data["newsLink3"]
+
+print(companyDescription, "\n")
+
+print(logoURL, "\n")
+
+print(newsArticle1, "\n")
+print(newsArticle2, "\n")
+print(newsArticle3, "\n")
+
+print(newsLink1, "\n")
+print(newsLink2, "\n")
+print(newsLink3, "\n")
+
+print(currentPrice, "\n")
